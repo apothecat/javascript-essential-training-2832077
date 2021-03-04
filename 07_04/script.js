7450debdfadb661be725664f60d9709f0a4d883c
Catherine_McGuire <catherine.mcguire@manchester.ac.uk>
2021-03-04 18:45:23 +0000

    Build and modify an array.

--- 07_04/script.js
+++ 07_04/script.js
@@ -2,10 +2,50 @@
  * Challenge: Build and modify an array
  * - Build an array with 8 items
  * - Remove the last item
- * - Add the last item as the first item on the array 
+ * - Add the last item as the first item on the array
  * - Sort the items by alphabetical order
  * - Use the find() method to find a specific item in the array
  * - Remove the item you found using the find method from the array.
  */
 
+// Build an array with 8 items
 
+const myArray = [
+  "lamp",
+  "notebook",
+  "mouse",
+  "nuts",
+  "phone",
+  "mug",
+  "glass",
+  "pen",
+];
+console.log(myArray);
+
+// Remove the last item
+
+myArray.pop();
+
+console.log(myArray);
+
+// Add the last item as the first item on the array
+
+myArray.unshift(myArray.pop());
+
+console.log(myArray);
+
+// Sort the items by alphabetical order
+
+myArray.sort();
+
+console.log(myArray);
+
+// Use the find() method to find a specific item in the array
+
+const found = myArray.find((item) => item === "mug");
+//const foundIndex = myArray.findIndex((item) => item === "mug");
+console.log("item:", found);
+// Remove the item you found using the find method from the array.
+
+const myNewArray = myArray.filter((item) => item != "mug");
+console.log(myNewArray);
